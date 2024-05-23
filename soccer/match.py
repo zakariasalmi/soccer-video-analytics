c0d35619ed91faf452beaeb54a6398f34fe4e04e
zakariasalmi <zakaria.salmi123@gmail.com>
2024-05-23 17:22:41 +0100

    ressize

--- soccer/match.py
+++ soccer/match.py
@@ -606,7 +606,7 @@ class Match:
 
         # get width of PIL.Image
         frame_width = frame.size[0]
-        counter_origin = (frame_width - 300, 20)
+        counter_origin = (frame_width - 320, 20)
 
         frame = self.draw_counter_background(
             frame,
@@ -680,7 +680,7 @@ class Match:
 
         # get width of PIL.Image
         frame_width = frame.size[0]
-        counter_origin = (frame_width - 300, 90)
+        counter_origin = (frame_width - 320, 290)
 
         frame = self.draw_counter_background(
             frame,
